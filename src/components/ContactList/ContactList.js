44d734c5878ba69e77ac180382184e7dd76654a5
Daniel Holmberg <danielholmberg.dev@gmail.com>
2017-12-10 02:44:25 +0100

    added m'date' chip and made some small design changes.

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -3,7 +3,6 @@ import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc'
 
 import Avatar from 'material-ui/Avatar';
 import SocialPerson from 'material-ui/svg-icons/social/person';
-import SocialPeople from 'material-ui/svg-icons/social/people';
 
 import {List, ListItem} from 'material-ui/List';
 import Chip from 'material-ui/Chip';
@@ -12,27 +11,56 @@ import Chip from 'material-ui/Chip';
 
 // STYLES
 const styles = {
+  listItem: {
+    textAlign: 'left',
+    backgroundColor: '#FFFFFFCC' // 80% Transparency
+  },
   chip: {
     margin: 4,
   },
+  chipLabel: {
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+  dateChip: {
+    margin: 4,
+    backgroundColor: '#FFFFFF00', // 100% Transparency
+  },
+  dateChipLabel: {
+    fontSize: 10,
+    fontWeight: 'bold',
+  },
+  dateChipLabelColor: '#00000080',  // 50% Transparency
+  chipWrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 };
 
-function handleClick({company}) {
-  // TODO - Should open Modal with all Contacts related to 'company'.
-  alert('You clicked ' + company);
-}
+function onContextClick({context}) {
+  // TODO - Should open a modal with a list of all contacts contected to incoming 'context'.
+  alert('You clicked on context: ' + context);
+};
 
 // LIST ITEM
 const SortableItem = SortableElement(({collection, contact}) => {
-  console.log(contact.name + ': ' + contact.company);
-  const company = contact.company;
+  //const company = contact.company;
+  const context = contact.context;
+  const latest_dialogue = contact.latest_dialogue;
   return (
     <ListItem
-      style={{textAlign: 'left', backgroundColor: 'white',}}
+      disabled={true}
+      style={styles.listItem}
       leftAvatar={<Avatar icon={<SocialPerson />}/>}
       primaryText={contact.name}
       secondaryText={contact.id}
-      rightAvatar={<Chip style={styles.chip} onClick={() => handleClick({company})}>{company}</Chip>}
+      rightAvatar={
+        <div style={styles.chipWrapper}>
+          <Chip style={styles.chip} labelStyle={styles.chipLabel} onClick={() => onContextClick({context})}>{context}</Chip>
+          <Chip style={styles.dateChip} labelStyle={styles.dateChipLabel} labelColor={styles.dateChipLabelColor}>{latest_dialogue}</Chip>
+        </div>}
     />
   );
 });
@@ -40,7 +68,7 @@ const SortableItem = SortableElement(({collection, contact}) => {
 // LIST
 const SortableList = SortableContainer(({contacts}) => {
   return (
-    <List style={{flex:1, flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center'}}>
+    <List>
       {contacts.map((contact, index) => {
         return (
           <SortableItem key={contact.id} index={index} collection={contact.company} contact={contact} />
@@ -56,25 +84,41 @@ export default class ContactList extends Component {
     contacts: [
       {
         'id':'123@gmail.com',
-        'name':'Daniel',
+        'name':'Daniel Holmberg',
         'company':'Sectra AB',
+        'context':'STABEN Spons',
+        'latest_dialogue':'2017-12-09',
+        'info':'Blablabla blabla blablabla.',
       },
       {
         'id':'234@gmail.com',
-        'name':'Albin',
+        'name':'Albin Andersson',
         'company':'Sectra AB',
+        'context':'STABEN Spons',
+        'latest_dialogue':'2017-12-08',
+        'info':'Blablabla blabla blablabla.',
       },
       {
         'id':'345@gmail.com',
-        'name':'Christian',
+        'name':'Christian Holmberg',
         'company':'Accenture',
+        'context':'NärU',
+        'latest_dialogue':'2017-12-02',
+        'info':'Blablabla blabla blablabla.',
       },
       {
         'id':'456@gmail.com',
-        'name':'Johanna',
+        'name':'Johanna Assarsson',
         'company':'Spotify',
+        'context':'D-LAN Spons',
+        'latest_dialogue':'2017-11-10',
+        'info':'Blablabla blabla blablabla.',
       },
     ],
+  };
+
+  onSortStart = ({oldIndex, newIndex}) => {
+
   };
 
   onSortEnd = ({oldIndex, newIndex}) => {
@@ -88,6 +132,9 @@ export default class ContactList extends Component {
     return (
       <SortableList
         contacts={this.state.contacts}
+        pressDelay={200}
+        transitionDuration={400}
+        onSortStart={this.onSortStart}
         onSortEnd={this.onSortEnd}
         useWindowAsScrollContainer={true}
         lockAxis='y'
